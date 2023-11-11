7310415d0726df34ad0ad28d67198966f089e0ae
osawa-koki <koki@osawa.dev>
2023-11-12 00:17:35 +0900

    サインアップ処理の実装。

---
+++ client/initializer.sh
@@ -0,0 +1,22 @@
+#!/bin/zsh
+
+project_name=$1
+if [ -z "$project_name" ]; then
+  echo "project_name is empty"
+  exit 1
+fi
+
+# 配列を定義
+declare -A array
+
+# 配列に値をセット
+array["UserPoolId"]=$(aws cloudformation describe-stacks --stack-name $project_name --query "Stacks[0].Outputs[?OutputKey=='UserPoolId'].OutputValue" --output text --no-cli-pager)
+array["UserPoolClientId"]=$(aws cloudformation describe-stacks --stack-name $project_name --query "Stacks[0].Outputs[?OutputKey=='UserPoolClientId'].OutputValue" --output text --no-cli-pager)
+
+# .envファイルを作成
+echo "" >> .env.local
+echo "# Generated automatically by initializer.sh" >> .env.local
+echo "# project_name: $project_name" >> .env.local
+echo "NEXT_PUBLIC_USER_POOL_ID=\"${array["UserPoolId"]}\"" >> .env.local
+echo "NEXT_PUBLIC_USER_POOL_CLIENT_ID=\"${array["UserPoolClientId"]}\"" >> .env.local
+echo "" >> .env.local
--- client/pages/sign_up.tsx
+++ client/pages/sign_up.tsx
@@ -1,9 +1,61 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Button, Form } from 'react-bootstrap'
+import makeCognitoUserAttributes from '../src/common/makeCognitoUserAttributes'
+import { userPool } from '../src/common/cognito'
 
 export default function SignUpPage (): React.JSX.Element {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const signUp = async () => {
+    const attributeList = makeCognitoUserAttributes({
+      name,
+      email
+    })
+
+    userPool.signUp(
+      email,
+      password,
+      attributeList,
+      attributeList,
+      (err, result) => {
+        // 登録がエラーとなった場合の処理
+        if (err != null) {
+          console.error(err)
+          return
+        }
+
+        // 登録が成功した場合の処理
+        if (result != null) {
+          const cognitoUser = result.user
+          console.log('user name is ' + cognitoUser.getUsername())
+        } else {
+          console.error('result is null')
+        }
+      }
+    )
+  }
+
   return (
     <>
       <div id='SignUpbout'>
+        <Form.Group controlId='formBasicName' className='mt-3'>
+          <Form.Label>Your name</Form.Label>
+          <Form.Control type='text' placeholder='Enter name' value={name} onInput={(event) => setName(event.currentTarget.value)} />
+        </Form.Group>
+        <Form.Group controlId='formBasicEmail' className='mt-3'>
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type='email' placeholder='Enter email' value={email} onInput={(event) => setEmail(event.currentTarget.value)} />
+        </Form.Group>
+        <Form.Group controlId='formBasicPassword' className='mt-3'>
+          <Form.Label>Password</Form.Label>
+          <Form.Control type='password' placeholder='Password' value={password} onInput={(event) => setPassword(event.currentTarget.value)} />
+        </Form.Group>
+        <hr />
+        <Button variant='primary' className='mt-3' onClick={signUp}>
+          Sign Up
+        </Button>
       </div>
     </>
   )
