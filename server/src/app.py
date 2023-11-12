3e7be11113cd573287b0b16a8a72966d0207d6ef
osawa-koki <koki@osawa.dev>
2023-11-12 20:09:56 +0900

    サーバサイド処理の実装。

--- server/src/app.py
+++ server/src/app.py
@@ -5,15 +5,20 @@ from typing import Union
 
 from fastapi import FastAPI, Header, status
 from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 from mangum import Mangum
 
 from initializer import initializer
 from cognito_client import cognito_client
 
-app = FastAPI()
-
 initializer()
-
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 COGNITO_CLIENT_ID = environ["USER_POOL_CLIENT_ID"]
 
 
@@ -24,7 +29,7 @@ def read_root():
     Returns:
         dict: `Hello World`を含む辞書データ。
     """
-    return {"Hello": "World"}
+    return {"message": "Hello World"}
 
 
 @app.get("/api/envs")
@@ -41,50 +46,24 @@ def read_envs():
 def verify_jwt(authorization: Union[str, None] = Header(default=None)):
     """JWTを検証する。
     """
-    if (authorization is None) or (len(authorization.split(" ")) != 2):
+    splitted_authorization = authorization.split(" ")
+    if (authorization is None) or (len(splitted_authorization) != 2):
         content = {"message": "Invalid header."}
         return JSONResponse(
             status_code=status.HTTP_400_BAD_REQUEST,
             content=content,
         )
-    jwt = authorization.split(" ")[1]
+    jwt = splitted_authorization[1]
     try:
         response = cognito_client.get_user(
             AccessToken=jwt,
         )
-        name = [attr["Value"]
-                for attr in response["UserAttributes"] if attr["Name"] == "name"][0]
-        email = [attr["Value"] for attr in response["UserAttributes"]
-                 if attr["Name"] == "email"][0]
-        comment = [attr["Value"] for attr in response["UserAttributes"]
-                   if attr["Name"] == "custom:comment"][0]
-        content = {
-            "message": "success.",
-            "user": {
-                "sub": response["Username"],
-                "name": name,
-                "email": email,
-                "comment": comment,
-            },
-        }
         return JSONResponse(
             status_code=status.HTTP_200_OK,
-            content=content,
+            content=response,
         )
     except cognito_client.exceptions.NotAuthorizedException:
         content = {"message": "Not authorized."}
-        return JSONResponse(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            content=content,
-        )
-    except cognito_client.exceptions.UserNotFoundException:
-        content = {"message": "User not found."}
-        return JSONResponse(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            content=content,
-        )
-    except cognito_client.exceptions.InvalidParameterException:
-        content = {"message": "Invalid parameter."}
         return JSONResponse(
             status_code=status.HTTP_400_BAD_REQUEST,
             content=content,
